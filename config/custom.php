6979f5dc4cfbc4d484ae7e205bd016cfdc5cae51
rashidweb <rashidk.developer@gmail.com>
2025-03-29 00:14:47 +0530

    setup env for landing page dynamic

--- config/custom.php
+++ config/custom.php
@@ -1,5 +1,7 @@
 <?php
 
-    return [
-        'backend_access_domain' => env('custom.backend_access_domain', '127.0.0.1'),
-    ];
+return [
+    'backend_access_domain' => env('BACKEND_ACCESS_DOMAIN'),
+    'school_id' => env('SCHOOL_ID'),
+    'assets_url' => env('ASSETS_URL'),
+];
