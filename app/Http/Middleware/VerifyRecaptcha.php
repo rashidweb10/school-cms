adaa5fd4493d051484081fceedbd7067f5d3dbf0
rashidweb <rashidk.developer@gmail.com>
2025-04-12 15:40:45 +0530

    fixing log

--- app/Http/Middleware/VerifyRecaptcha.php
+++ app/Http/Middleware/VerifyRecaptcha.php
@@ -16,7 +16,7 @@ class VerifyRecaptcha
         $action = $request->input('recaptcha_action');
 
         if (!$token || !$action) {
-            // Log::warning('Missing reCAPTCHA token or action.');
+            Log::warning('Missing reCAPTCHA token or action.');
             abort(403, 'ReCAPTCHA verification failed.');
         }
 
@@ -30,10 +30,10 @@ class VerifyRecaptcha
         Log::info('reCAPTCHA response', $data);
 
         if (!$data['success'] || $data['action'] !== $action || $data['score'] < 0.5) {
-            // Log::warning('reCAPTCHA failed', [
-            //     'score' => $data['score'] ?? 'N/A',
-            //     'action' => $data['action'] ?? 'N/A',
-            // ]);
+            Log::warning('reCAPTCHA failed', [
+                'score' => $data['score'] ?? 'N/A',
+                'action' => $data['action'] ?? 'N/A',
+            ]);
             abort(403, 'reCAPTCHA verification failed.');
         }
 
