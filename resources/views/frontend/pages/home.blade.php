bc1e7cff2d5e6471f248b53435833503752077ca
Mahtab645 <ansarimehtab645@gmail.com>
2025-04-19 22:06:19 +0530

    change banner slider time

--- resources/views/frontend/pages/home.blade.php
+++ resources/views/frontend/pages/home.blade.php
@@ -31,7 +31,7 @@
 @endphp
 
 <div class="banner_slider">
-      <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel" data-bs-interval="3000">
+      <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel" data-bs-interval="7000">
         <!-- Carousel items -->
         <div class="carousel-inner">
         @foreach($banner_images as $index => $id)
