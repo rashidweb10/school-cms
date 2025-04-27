1907ea4b1bf8d4cb8bec939e4403025dcd55a299
rashidweb <rashidk.developer@gmail.com>
2025-04-27 12:15:07 +0530

    added search

--- resources/views/frontend/pages/results.blade.php
+++ resources/views/frontend/pages/results.blade.php
@@ -251,9 +251,18 @@
 
 @section('scripts')
 
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<link href="https://cdn.datatables.net/1.13.5/css/jquery.dataTables.min.css" rel="stylesheet">
+<script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
 <script>
 $(document).ready(function() {
+
+    $('#dataTable').DataTable({
+        "paging": true,
+        "info": true,
+        "ordering": true,
+        "searching": true
+    });
+
     // Step 1: Extract available years from non-blank year cells
     var years = new Set();
     $('#dataTable tbody tr').each(function() {
