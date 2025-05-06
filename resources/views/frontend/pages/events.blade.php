f20331ece0d0eb852f8ceeb64621494ab3199644
Mahtab645 <ansarimehtab645@gmail.com>
2025-05-06 21:16:10 +0530

    event page responsive

--- resources/views/frontend/pages/events.blade.php
+++ resources/views/frontend/pages/events.blade.php
@@ -230,7 +230,7 @@ $(document).ready(function () {
     let skeletonHTML = '';
 for (let i = 0; i < 4; i++) { // show 4 skeleton boxes
   skeletonHTML += `
-    <div class="event_box col-md-3 skeleton">
+    <div class="event_box col-md-3 col-6 skeleton">
       <a class="bounce" style="pointer-events: none;">
         <div class="img-placeholder"></div>
       </a>
