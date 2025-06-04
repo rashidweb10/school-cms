3ea872887cd853717edaa336fb48e60deedbf868
rashidweb10 <rashidk.developer@gmail.com>
2025-06-04 23:37:54 +0530

    .

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Form;
 use App\Mail\FormSubmissionMail;
 use Illuminate\Support\Facades\Mail;
+use App\Models\Company;
 
 class FormController extends Controller
 {
@@ -17,6 +18,8 @@ class FormController extends Controller
         $validatedData = $request->validate($validationRules);
         $formData = collect($validatedData)->except(['form_name', 'name', 'email', 'phone'])->toArray();
 
+        $companyId = $request->input('company_id') ?? config('custom.school_id');
+
         $form = Form::create([
             'form_name' => $formName,
             'name' => $request->input('name'),
@@ -24,11 +27,24 @@ class FormController extends Controller
             'phone' => $request->input('phone'),
             'form_data' => $formData,
             'ip' => request()->ip(),
-            'company_id' => $request->input('company_id') ?? config('custom.school_id') //config('custom.school_id'),
+            'company_id' => $companyId //config('custom.school_id'),
         ]);
 
+        $company = Company::with(['meta' => function ($q) {
+            $q->whereIn('meta_key', ['general_enquiry', 'admission_enquiry']);
+        }])->where('id', $companyId)->first();
+
+        $generalEnquiry = $company->meta->where('meta_key', 'general_enquiry')->first()->meta_value ?? config('mail.from.address');
+        $admissionEnquiry = $company->meta->where('meta_key', 'admission_enquiry')->first()->meta_value ?? config('mail.from.address');
+
+        // Determine recipient email
+        $recipientEmail = ($request->filled('enquiry_type') && $request->input('enquiry_type') === 'Admission')
+            ? $admissionEnquiry
+            : $generalEnquiry;        
+        
         Mail::to(config('mail.from.address'))
             ->queue(new FormSubmissionMail($formName, $validatedData));
+            
 
         // Mail::to(config('mail.from.address'))
         // ->send(new FormSubmissionMail($formName, $validatedData));        
