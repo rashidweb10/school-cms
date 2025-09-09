98d0c18a74a734a357f1105cb0c7531191d4ba24
rashidweb10 <rashidk.developer@gmail.com>
2025-09-09 20:40:08 +0530

    .

--- resources/views/frontend/partials/breadcrumb.blade.php
+++ resources/views/frontend/partials/breadcrumb.blade.php
@@ -18,7 +18,10 @@
         <a target="_blank" href="{{get_setting('admission_banner_url')}}">
           <img class="hvr-bounce-in w-100 admission_img bounce_continue" src="{{ central_asset(uploaded_asset(get_setting('admission_banner'))) }}" alt="img">
         </a>
+        @if(get_setting('admission_year'))
+          <p><b>{{get_setting('admission_year')}}</b></p>
         </div>
+        @endif
       </div>
     </div>
   </div>
