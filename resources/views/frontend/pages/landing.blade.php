721902fcd82c5eab677a23133cf370126d385e4f
rashidweb10 <rashidk.developer@gmail.com>
2025-09-21 16:48:26 +0530

    .

--- resources/views/frontend/pages/landing.blade.php
+++ resources/views/frontend/pages/landing.blade.php
@@ -518,7 +518,7 @@ $(document).ready(function () {
         } else if (city === 'Navi Mumbai') {
             $allOptions.filter(function () {
                 //console.log($(this).attr('data-short-name'));
-                return $(this).attr('data-short-name') === 'nhssa' || $(this).attr('data-short-name') === 'nhpsa' || $(this).attr('data-short-name') === 'nhpsa3';
+                return $(this).attr('data-short-name') === 'nhssa' || $(this).attr('data-short-name') === 'nhpsa' || $(this).attr('data-short-name') === 'nhpsas3';
             }).show();
         } else if (city === 'Panvel') {
             $allOptions.filter(function () {
