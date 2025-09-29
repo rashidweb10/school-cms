a95d4b510d8c8b57c32eb21626ccc990c8914c7d
Mahtab645 <ansarimehtab645@gmail.com>
2025-09-29 21:00:28 +0530

    give space

--- resources/views/frontend/pages/landing.blade.php
+++ resources/views/frontend/pages/landing.blade.php
@@ -262,7 +262,7 @@
 
           
           <div class="col-lg-6" data-aos="fade-left" data-aos-duration="1000" data-aos-once="true">
-            <div class="text-start mb-md-4 mb-2 pt-4">
+            <div class="text-start mb-md-4 mb-2 pt-4 ps-md-5">
               
               <h3 class="roboto text_color roboto padding100">{{$about_title}}</h3>
             </div>
