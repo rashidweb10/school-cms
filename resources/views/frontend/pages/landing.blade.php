f060eeade1ae165f0dcc9bdff3cd33a03261264a
Mahtab645 <ansarimehtab645@gmail.com>
2025-11-06 20:10:14 +0530

    jkj

--- resources/views/frontend/pages/landing.blade.php
+++ resources/views/frontend/pages/landing.blade.php
@@ -55,7 +55,7 @@
       <div class="container position-relative">
         <div class="row">
           <div class="col-lg-7">
-            <p class="foranyclass"><b>For AY 2025-2026 directly fill the form <span>from respective school website.</span></b></p>
+            <p class="foranyclass"><b>For AY 2025-2026 directly fill the form from respective school website.</b></p>
             <div class="text-start mb-md-4 mb-2 pt-4">
               <div class="skew-box ">
                   <p class="roboto text_color"> {!! $banner_title !!} </p>
@@ -76,20 +76,14 @@
                  <!-- <p class="foranyclass"><b>For AY 2025-2026 directly fill the form from respective school website.</b></p> -->
                 <style>
                  .foranyclass {
-    font-size: 16px;
+   font-size: 15px;
     line-height: 36px;
     position: absolute;
-    font-weight: 500;
+    font-weight: 400;
     margin-top: -161px;
-    width: 573px;
+    width: 540px;
     background: #fff;
-    padding: 4px 30px;
-}
-                 .foranyclass span {
-    /* display: block; */
-    font-size: 16px;
-    line-height: 22px;
-    font-weight: bold;
+    padding: 2px 20px;
 }
                 </style>
 
