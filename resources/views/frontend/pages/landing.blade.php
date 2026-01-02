bd265a4169d41cce0f4d0fb73dd509ecf729eb2c
Muhammed rashid <rashidk.developer@gmail.com>
2026-01-02 22:02:28 +0530

    .

--- resources/views/frontend/pages/landing.blade.php
+++ resources/views/frontend/pages/landing.blade.php
@@ -198,12 +198,12 @@
                       <div class="form-outline">
                         <select name="academic_year" id="academic_year" class="form-control" required>
                             <option value="">--- Select Academic Year ---</option>
-                            <!-- <option value="{{ $currentYear }}-{{ $nextYear }}">
+                            <option value="{{ $currentYear }}-{{ $nextYear }}">
                                 {{ $currentYear }}-{{ $nextYear }}
-                            </option> -->
-                            <option value="{{ $nextYear }}-{{ $secondNextYear }}">
-                                {{ $nextYear }}-{{ $secondNextYear }}
                             </option>
+                            <!-- <option value="{{ $nextYear }}-{{ $secondNextYear }}">
+                                {{ $nextYear }}-{{ $secondNextYear }}
+                            </option> -->
                         </select>
                       </div>
                     </div>
