20def4bc475a1e003dd2062a32704700f5f03566
Muhammed rashid <rashidk.developer@gmail.com>
2026-07-01 20:40:41 +0530

    Resume fixed in template

--- resources/views/emails/form_submission.blade.php
+++ resources/views/emails/form_submission.blade.php
@@ -4,8 +4,13 @@
 # {{ ucfirst($formName) }} Form Submission
 
 @foreach($data as $key => $value)
+@if($key === 'resume')
+**{{ ucwords(str_replace('_', ' ', $key)) }}:** <a href="{{ asset('storage/' . $value) }}" style="display:inline-block;padding:8px 16px;background-color:#007bff;color:#ffffff;text-decoration:none;border-radius:4px;">View Resume</a>
+
+@else
 **{{ ucwords(str_replace('_', ' ', $key)) }}:** {{ $value }}
 
+@endif
 @endforeach
 
 Thanks,<br>
