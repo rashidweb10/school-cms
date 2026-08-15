1af7af8c471e811074e5d1204556c52f207fdcf5
Muhammed rashid <rashidk.developer@gmail.com>
2026-08-15 11:17:59 +0530

    Fix brochure link rendering in footer by adjusting conditional statement

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -13,12 +13,12 @@
               <li><a href="{{route('events')}}"> Events</a></li>
               <li><a href="{{route('curriculum')}}"> Curriculum</a></li>
               <li><a href="{{route('alumini')}}"> Alumni</a></li>
+              @endif
               <li>
                   <a target="_blank" href="{{ get_setting('brochure_url') ?: central_asset(uploaded_asset(get_setting('brochure_attachment', '#'))) }}">
                       BROCHURE
                   </a>
               </li>              
-              @endif
             </ul>
             <ul class="footer-menu">
               
