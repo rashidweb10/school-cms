febde21f2905af10624697c63cf3cc913d35f8d8
Muhammed rashid <rashidk.developer@gmail.com>
2026-09-12 12:14:50 +0530

    Update phone number label in contact page for accuracy

--- resources/views/frontend/pages/contact.blade.php
+++ resources/views/frontend/pages/contact.blade.php
@@ -31,7 +31,7 @@
               <div>
                 <h5 class="mb-1 fs16">Phone Number:</h5>
                 <p class="mb-0">
-                      <u>Admission Counselling:</u> <a href="tel:7738292703" class=" text-decoration-none" style="color:#000;">{{get_setting('phone')}}  </a>
+                      <u>Admission Office:</u> <a href="tel:7738292703" class=" text-decoration-none" style="color:#000;">{{get_setting('phone')}}  </a>
                 </p>
                  <p class="mb-0">
                       <u>Office:</u><a href="tel:022-25972729" class=" text-decoration-none" style="color:#000;"> {{get_setting('phone2')}}</a>
